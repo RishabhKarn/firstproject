1cc2f2d960942d1335901838205893f92a1e6e83
RishabhKarn <rishabhranjankarn@gmail.com>
2017-11-16 11:31:29 +0530

    Add files via upload

---
+++ FirstProject.java
@@ -0,0 +1,14 @@
+package testmaven;
+
+public class FirstProject {
+
+	public static void main(String[] args) {
+		int x,y,z;
+		x=4;
+		y=5;
+		z=x+y;
+		System.out.println("The sum of "+x+" and "+y+" is "+z);
+
+	}
+
+}
